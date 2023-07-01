4a05da62e647eccd7f533d2ced05776a9a9d24bd
Rei Urbano <130446376+reiurbano@users.noreply.github.com>
2023-07-01 13:07:11 +0800

    fixed form styles

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,24 +4,24 @@ function Login() {
     return (
         <div className="mx-5 mt-5 py-3 border rounded" id="login">
             <h1 className="mx-5 my-2">Login</h1><hr></hr>
-            <form id="loginForm" className="mt-3 mx-5">
-                <div className="row mt-2 justify-content-between">
-                    <div className="col-md-4">
+            <form className="mt-3 mx-5" id="loginForm">
+                <div className="row mt-2">
+                    <div className="col-lg-5 my-2 mx-2">
                         <label for="email" className="form-label">Email:</label>
-                        <input id="email" className="form-control" type="email" required />
+                        <input id="email" className="form-control p-1" type="email" required />
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-lg-5 my-2 mx-2">
                         <label for="password" className="form-label">Password:</label>
-                        <input id="password" className="form-control" type="password" required />
+                        <input id="password" className="form-control p-1" type="password" required />
                     </div>
-                    <div className="col-md-2 align-self-end">
-                        <button className="btn btn-primary mt-3 w-100">Login</button>
-                    </div>
-                    <div className="col-md-4 py-2">
-                        <Link to="/register" className="p-2">Don't have an account?</Link>
+                    <div className="col-lg-1 my-2 mx-auto align-self-end">
+                        <button className="btn btn-dark border py-1 mx-2 w-100">Login</button>
                     </div>
                 </div>
             </form>
+            <div className="mx-5 my-2">
+                <Link to="/register" className="px-2 py-2">Don't have an account?</Link>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,32 +4,32 @@ function Register() {
     return (
         <div className="mx-5 mt-5 py-3 border rounded" id="register">
             <h1 className="mx-5 my-2">Register</h1><hr></hr>
-            <form id="registrationForm" className="mt-3">
-                <div className="row mt-2 mx-5 justify-content-between">
-                    <div className="col-lg-5">
+            <form className="mt-3 mx-5" id="registrationForm">
+                <div className="row mt-2">
+                    <div className="col-lg-5 mx-2 my-2">
                         <label for="username" className="form-label">Username:</label>
-                        <input id="username" className="form-control" type="text" required />
+                        <input id="username" className="form-control p-1" type="text" required />
                     </div>
-                    <div className="col-lg-5">
+                    <div className="col-lg-5 mx-2 my-2">
                         <label for="email" className="form-label">Email:</label>
-                        <input id="email" className="form-control" type="email" required />
+                        <input id="email" className="form-control p-1" type="email" required />
                     </div>
-                </div>
-                <div className="row mt-2 mx-5 justify-content-between">
-                    <div className="col-md-3">
+                    <div className="col-lg-4 mx-2 my-2">
                         <label for="password" className="form-label">Password:</label>
-                        <input id="password" className="form-control" type="password" required />
+                        <input id="password" className="form-control p-1" type="password" required />
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-lg-4 mx-2 my-2">
                         <label for="confirm_password" className="form-label">Confirm Password:</label>
-                        <input id="confirm_password" className="form-control" type="password" required />
+                        <input id="confirm_password" className="form-control p-1" type="password" required />
                     </div>
-                    <div className="col-md-4 mt-2">
-                        <button className="btn btn-primary p-1 px-2 mt-2 mx-2">Submit</button>
-                        <Link to="/login" className="align-bottom py-2">Already have an account?</Link>
+                    <div className="col-lg-2 my-2 me-auto align-self-end">
+                        <button className="btn btn-dark border py-1 mx-2 w-100">Register</button>
                     </div>
                 </div>
             </form>
+            <div className="mx-5 my-2">
+                <Link to="/login" className="px-2 py-2">Already have an account?</Link>
+            </div>
         </div>
     )
 }
