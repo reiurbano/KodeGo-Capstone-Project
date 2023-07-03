91d062018885146917831df2e243cf1b8def4f1a
Rei Urbano <130446376+reiurbano@users.noreply.github.com>
2023-07-04 02:52:43 +0800

    can add cards to deck

--- frontend/src/Deck.jsx
+++ frontend/src/Deck.jsx
@@ -1,7 +1,14 @@
-function Deck() {
+function Deck({ chapter, cost, dice, effect, id, range, rarity, title, source, index }) {
     return (
-        <div>
-
+        <div className="accordion-item">
+            <h2 className="accordion-header">
+                <button className="accordion-button collapsed py-3 px-4" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse-${index}`} aria-expanded="false" aria-controls={`flush-collapse-${index}`}>
+                    {title}
+                </button>
+            </h2>
+            <div id={`flush-collapse-${index}`} className="accordion-collapse collapse" data-bs-parent="#deckAccordion">
+                <div className="accordion-body p-2">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+            </div>
         </div>
     )
 }
--- frontend/src/DeckView.jsx
+++ frontend/src/DeckView.jsx
@@ -1,45 +1,40 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { DeckContext } from "./Contexts"
+import Deck from "./Deck";
 
 function DeckView() {
     const deck = useContext(DeckContext);
+    const [temp, updateTemp] = useState([]);
 
     useEffect(() => {
-        console.log(deck.deck);
+        updateTemp(deck.deck);
     }, [deck.deck])
+
+    const mapDeck = () => {
+        if (temp != undefined && temp.length != 0) {
+            const deckList = temp.map((item,index) => {
+                return <Deck
+                    chapter={item.chapter}
+                    cost={item.cost}
+                    dice={item.dice}
+                    effect={item.effect}
+                    key={index}
+                    id={item.id}
+                    range={item.range}
+                    rarity={item.rarity}
+                    title={item.title}
+                    source={item.source}
+                    index={index}
+                />;
+            });
+
+            return deckList;
+        }
+    }
 
     return (
         <div className="accordion accordion-flush mx-2" id="deckAccordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed py-3 px-4" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                        Accordion Item #1
-                    </button>
-                </h2>
-                <div id="flush-collapseOne" className="accordion-collapse collapse show" data-bs-parent="#deckAccordion">
-                    <div className="accordion-body p-2">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-                </div>
-            </div>
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed py-3 px-4" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        Accordion Item #2
-                    </button>
-                </h2>
-                <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#deckAccordion">
-                    <div className="accordion-body p-2">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-                </div>
-            </div>
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed py-3 px-4" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                        Accordion Item #3
-                    </button>
-                </h2>
-                <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#deckAccordion">
-                    <div className="accordion-body p-2">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-                </div>
-            </div>
+            {mapDeck()}
         </div>
     )
 }
